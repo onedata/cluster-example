97fad71ac03593f956677a1b12b327147d479354
Michal Zmuda <zmu.michal@gmail.com>
2016-02-06 16:22:46 +0100

    Updated example to work with new CW

--- src/modules/rest/rest_listener.erl
+++ src/modules/rest/rest_listener.erl
@@ -20,7 +20,7 @@
 -behaviour(listener_behaviour).
 
 %% listener_starter_behaviour callbacks
--export([start/0, stop/0]).
+-export([start/0, stop/0, port/0, healthcheck/0]).
 
 %%%===================================================================
 %%% listener_starter_behaviour callbacks
@@ -33,24 +33,24 @@
 %%--------------------------------------------------------------------
 -spec start() -> ok | {error, Reason :: term()}.
 start() ->
-  Dispatch = [
-    {'_', [
-      {"/example/[...]", rest_handler, []}
-    ]}
-  ],
+    Dispatch = [
+        {'_', [
+            {"/example/[...]", rest_handler, []}
+        ]}
+    ],
 
-  % Start the listener for nagios handler
-  Result = cowboy:start_http(?REST_LISTENER, 5,
-    [
-      {ip, {0, 0, 0, 0}},
-      {port, 8080}
-    ], [
-      {env, [{dispatch, cowboy_router:compile(Dispatch)}]}
-    ]),
-  case Result of
-    {ok, _} -> ok;
-    _ -> Result
-  end.
+    % Start the listener for nagios handler
+    Result = cowboy:start_http(?REST_LISTENER, 5,
+        [
+            {ip, {0, 0, 0, 0}},
+            {port, port()}
+        ], [
+            {env, [{dispatch, cowboy_router:compile(Dispatch)}]}
+        ]),
+    case Result of
+        {ok, _} -> ok;
+        _ -> Result
+    end.
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -59,13 +59,37 @@ start() ->
 %%--------------------------------------------------------------------
 -spec stop() -> ok | {error, Reason :: term()}.
 stop() ->
-  case catch cowboy:stop_listener(?REST_LISTENER) of
-    (ok) ->
-      ok;
-    (Error) ->
-      ?error("Error on stopping listener ~p: ~p", [?REST_LISTENER, Error]),
-      {error, rest_stop_error}
-  end.
+    case catch cowboy:stop_listener(?REST_LISTENER) of
+        (ok) ->
+            ok;
+        (Error) ->
+            ?error("Error on stopping listener ~p: ~p", [?REST_LISTENER, Error]),
+            {error, rest_stop_error}
+    end.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Returns the status of a listener.
+%% @end
+%%--------------------------------------------------------------------
+-spec healthcheck() -> ok | {error, server_not_responding}.
+healthcheck() ->
+    case http_client:get("http://127.0.0.1:" ++ integer_to_list(port()), [], <<>>, [insecure]) of
+        {ok, _, _, _} ->
+            ok;
+        _ ->
+            {error, server_not_responding}
+    end.
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% {@link listener_starter_behaviour} callback port/0.
+%% @end
+%%--------------------------------------------------------------------
+-spec port() -> integer().
+port() ->
+    8080.
 
 %%%===================================================================
 %%% Internal functions
